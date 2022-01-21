b19cc1dcb06dd7a32d2936a6ebf90c85312c47e6
Jose A Diaz Jr <Jad6993@psu.edu>
2022-01-21 00:05:02 -0500

    Initial commit

---
+++ js/custom.js
@@ -0,0 +1,24 @@
+/*! Initializing Materialize Components */
+
+/*! Initialize Mobile Collapse Navbar */
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.sidenav');
+  var instances = M.Sidenav.init(elems);
+});
+
+/*! Initialize Carousel */
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.carousel.carousel-slider');
+  var instance = M.Carousel.init(elems, {
+    fullWidth: true,
+    indicators: true
+  });
+});
+
+/*! Initialize Slider */
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.slider');
+  var instances = M.Slider.init(elems, {
+    height: 500
+  });
+});
